711557bc6804209e0c813bc585d9ba1e7535a1ab
Kasper Laudrup <laudrup@stacktrace.dk>
2018-07-11 13:53:08 +0200

    Added support for holidays API request
    Added support for the holidays API call that, given a start and end
    date, returns a dictionary with the date of the holiday as the key and
    the name and whether the day is a holiday as the value.

--- salling_group_holidays/api.py
+++ salling_group_holidays/api.py
@@ -12,14 +12,11 @@ class v1:
         self._api_key = api_key
         self._url = 'https://api.dansksupermarked.dk/v1/holidays'
 
-    def is_holiday(self, date):
-        if not isinstance(date, datetime.date):
-            raise SallingGroupHolidaysException("date must be a datetime.date instance")
-
+    def _make_request(self, params, path='/'):
         headers = {'Authorization': 'Bearer {}'.format(self._api_key)}
-        response = requests.get('{}/is-holiday'.format(self._url),
+        response = requests.get('{}/{}'.format(self._url, path),
                                 headers=headers,
-                                params={'date': date.isoformat()})
+                                params=params)
 
         if response.status_code != 200:
             if 'error' in response.json():
@@ -27,14 +24,34 @@ class v1:
             else:
                 raise SallingGroupHolidaysException(response.text)
 
-        reply = response.json()
+        return response
+
+    def is_holiday(self, date):
+        if not isinstance(date, datetime.date):
+            raise SallingGroupHolidaysException("date must be a datetime.date instance")
+
+        response = self._make_request({'date': date.isoformat()}, path='/is-holiday')
 
         # The API returns the boolean value as a string for some reason
-        if not isinstance(reply, str):
+        if not isinstance(response.json(), str):
             raise SallingGroupHolidaysException('Unexpected reply: {}'.format(response.text))
 
         # Convert the string to a boolean value
-        bool_reply = json.loads(reply)
+        bool_reply = json.loads(response.json())
         if not isinstance(bool_reply, bool):
             raise SallingGroupHolidaysException('Unexpected reply: {}'.format(response.text))
         return bool_reply
+
+    def holidays(self, start_date, end_date):
+        if not isinstance(start_date, datetime.date):
+            raise SallingGroupHolidaysException("start_date must be a datetime.date instance")
+        if not isinstance(end_date, datetime.date):
+            raise SallingGroupHolidaysException("end_date must be a datetime.date instance")
+
+        response = self._make_request({'startDate': start_date.isoformat(),
+                                       'endDate': end_date.isoformat()})
+        result = {datetime.datetime.strptime(val['date'], '%Y-%m-%d').date():
+                  {'name': val['name'],
+                   'holiday': val['nationalHoliday']}
+                  for val in response.json()}
+        return result
--- salling_group_holidays/tests/test_holidays.py
+++ salling_group_holidays/tests/test_holidays.py
@@ -6,6 +6,29 @@ from unittest import mock
 import salling_group_holidays
 from salling_group_holidays import SallingGroupHolidaysException
 
+DECEMBER_HOLIDAYS = [
+    {
+        'date': '2018-12-24',
+        'name': 'Juleaftensdag',
+        'nationalHoliday': True
+    },
+    {
+        'date': '2018-12-25',
+        'name': '1. juledag',
+        'nationalHoliday': True
+    },
+    {
+        'date': '2018-12-26',
+        'name': '2. juledag',
+        'nationalHoliday': True
+    },
+    {
+        'date': '2018-12-31',
+        'name': 'Nytårsaftensdag',
+        'nationalHoliday': True
+    }
+]
+
 
 def mocked_requests_get(url, headers=None, params=None):
     class MockResponse:
@@ -31,17 +54,37 @@ def mocked_requests_get(url, headers=None, params=None):
             return MockResponse('true', 200)
         return MockResponse('false', 200)
 
-    return MockResponse(None, 404)
+    return MockResponse(DECEMBER_HOLIDAYS, 200)
 
 
 class TestHolidays(unittest.TestCase):
     @mock.patch('requests.get', side_effect=mocked_requests_get)
     def test_invalid_key(self, mock_class):
         v1 = salling_group_holidays.v1('invalid_key')
+
         self.assertRaises(SallingGroupHolidaysException, v1.is_holiday, date(2018, 12, 24))
 
     @mock.patch('requests.get', side_effect=mocked_requests_get)
     def test_is_holiday(self, mock_class):
         v1 = salling_group_holidays.v1('valid_key')
+
         self.assertFalse(v1.is_holiday(date(2018, 12, 23)))
         self.assertTrue(v1.is_holiday(date(2018, 12, 24)))
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_holidays(self, mock_class):
+        v1 = salling_group_holidays.v1('valid_key')
+        result = v1.holidays(date(2018, 12, 1), date(2018, 12, 31))
+        days = [datetime.strptime(val['date'], '%Y-%m-%d').date() for val in DECEMBER_HOLIDAYS]
+
+        self.assertEquals(len(result), 4)
+        for day in days:
+            self.assertTrue(day in result)
+        self.assertEquals(result[date(2018, 12, 24)]['name'], 'Juleaftensdag')
+        self.assertTrue(result[date(2018, 12, 24)]['holiday'])
+        self.assertEquals(result[date(2018, 12, 25)]['name'], '1. juledag')
+        self.assertTrue(result[date(2018, 12, 25)]['holiday'])
+        self.assertEquals(result[date(2018, 12, 26)]['name'], '2. juledag')
+        self.assertTrue(result[date(2018, 12, 26)]['holiday'])
+        self.assertEquals(result[date(2018, 12, 31)]['name'], 'Nytårsaftensdag')
+        self.assertTrue(result[date(2018, 12, 31)]['holiday'])
